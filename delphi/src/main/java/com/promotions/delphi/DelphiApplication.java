06a1601a0574c87726d14f74bcbd81122d3a0e0f
sandilya12 <saisandilya22@gmail.com>
2020-10-26 22:45:58 +0530

    Adding intial files

---
+++ delphi/src/main/java/com/promotions/delphi/DelphiApplication.java
@@ -0,0 +1,13 @@
+package com.promotions.delphi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelphiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelphiApplication.class, args);
+	}
+
+}
---
+++ delphi/src/test/java/com/promotions/delphi/DelphiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotions.delphi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelphiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
